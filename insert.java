c43bedeb93aec1782f2eba07d5b61b3601a7a4b1
Dona Thomas <116492059+Donathomas10@users.noreply.github.com>
2023-02-24 15:12:15 +0530

    Update insert.java

--- insert.java
+++ insert.java
@@ -10,9 +10,9 @@ public class insert {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","mca","mca");
 			Statement stmt=con.createStatement();
-			String q=("update test set tid='t33' where tid='t03'");
+			String q=("insert into test values('t05','test5')");
 			stmt.executeUpdate(q); 
-			System.out.println("value updated");
+			System.out.println("values inserted");
 			con.close();
 		}catch(SQLException se) {System.out.println(se);}
 		catch(Exception e) {System.out.println(e);}
