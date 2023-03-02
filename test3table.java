02b0413b6ced60d03732d01c76bbe6efc516c24d
Dona Thomas <116492059+Donathomas10@users.noreply.github.com>
2023-03-02 14:45:59 +0530

    Add files via upload

---
+++ test3table.java
@@ -0,0 +1,15 @@
+package JDBCpgms;
+import java.sql.*;
+public class test3table {
+
+	public static void main(String[] args) {
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			connection con
+		}catch(Exception e) {System.out.println(e);}
+		
+
+	}
+
+}
